d084e3014967797de7e7ef1174b0b3513de5e7c7
harrdev <jasonharrphx@live.com>
2022-02-06 16:23:00 -0700

    added emailjs dependency

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,52 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillInstagram, AiFillFacebook, AiFillYoutube } from 'react-icons/ai';
+import { BsSpotify } from 'react-icons/bs'
 import { DiCssdeck } from 'react-icons/di';
-
+import { FcMusic } from 'react-icons/fc'
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
-const Header = () =>  (
+const Header = () => (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        <a style={{ display: 'flex', alignItems: 'center', color: "white", marginBottom: "20px" }}>
+          <FcMusic size="5rem" /> <span>Wailin' Calin</span>
         </a>
       </Link>
     </Div1>
     <Div2>
       <li>
         <Link href="#projects">
-          <NavLink>Projects</NavLink>
+          <NavLink>Videos</NavLink>
         </Link>
       </li>
       <li>
         <Link href="#tech">
-          <NavLink>Technologies</NavLink>
+          <NavLink>Gear</NavLink>
         </Link>
-      </li>        
+      </li>
       <li>
         <Link href="#about">
           <NavLink>About</NavLink>
         </Link>
-      </li>        
+      </li>
     </Div2>
-      <Div3>
-        <SocialIcons href="https://google.com">
-          <AiFillGithub size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="https://google.com">
-          <AiFillLinkedin size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="https://google.com">
-          <AiFillInstagram size="3rem"/>
-        </SocialIcons>
-      </Div3>
-    </Container>
+    <Div3>
+      <SocialIcons href="https://google.com">
+        <AiFillYoutube size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://google.com">
+        <AiFillFacebook size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://google.com">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="#">
+        <BsSpotify size="2.5rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
