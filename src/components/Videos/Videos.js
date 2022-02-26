7b4e383f6280511097c050233b795adb10676470
harrdev <jasonharrphx@live.com>
2022-02-25 18:35:24 -0700

    cleaned up unneeded import

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -7,10 +7,10 @@ const Videos = () => (
     <SectionDivider />
     <SectionTitle main>Videos</SectionTitle>
     <GridContainer>
-      <iframe width="360" height="215" src="https://www.youtube.com/embed/RKTeBlaxqIw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
-      <iframe width="360" height="215" src="https://www.youtube.com/embed/hNwJzFN1dq0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
-      <iframe width="360" height="215" src="https://www.youtube.com/embed/TbtVXTnbqrQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
-      <iframe width="360" height="215" src="https://www.youtube.com/embed/CnwzF5i4P20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+      <iframe width="360" height="215" src="https://www.youtube.com/embed/RKTeBlaxqIw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowfullscreen"></iframe>
+      <iframe width="360" height="215" src="https://www.youtube.com/embed/hNwJzFN1dq0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowfullscreen"></iframe>
+      <iframe width="360" height="215" src="https://www.youtube.com/embed/TbtVXTnbqrQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowfullscreen"></iframe>
+      <iframe width="360" height="215" src="https://www.youtube.com/embed/CnwzF5i4P20" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="allowfullscreen"></iframe>
     </GridContainer>
   </Section>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Hire from '../components/Hire/Hire';
 import Hero from '../components/Hero/Hero';
 import Videos from '../components/Videos/Videos';
 import Bands from '../components/Bands/Bands';
-import Timeline from '../components/TimeLine/TimeLine';
 import Contact from '../components/Contact/Contact';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
